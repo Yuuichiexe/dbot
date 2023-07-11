1ec699c5e9c54748267ea40b14b1f9f3d57b1815
Anonymous <adi855053@gmail.com>
2023-07-11 18:26:35 +0530

    Update afk.py

--- DazaiRobot/modules/afk.py
+++ DazaiRobot/modules/afk.py
@@ -173,6 +173,6 @@ dispatcher.add_handler(NO_AFK_HANDLER, AFK_GROUP)
 dispatcher.add_handler(AFK_REPLY_HANDLER, AFK_REPLY_GROUP)
 
 __help__ = """
-/afk reason - when someone will tag you bot will inform that user that you're afk.
+➛ /afk reason - when someone will tag you bot will inform that user that you're afk.
 """
-__mod_name__ = "Afk"
+__mod_name__ = "ᴀғᴋ"
