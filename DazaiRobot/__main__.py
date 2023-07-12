3587d5937fa1666703b25fcdfb3e46fb6ca58e67
Anonymous <adi855053@gmail.com>
2023-07-12 19:15:24 +0530

    Update __main__.py

--- DazaiRobot/__main__.py
+++ DazaiRobot/__main__.py
@@ -210,9 +210,9 @@ def start(update: Update, context: CallbackContext):
             
             time.sleep(0.1)
             lol.edit_text("💥")
-            time.sleep(0.2)
+            time.sleep(0.4)
             lol.edit_text("⚡")
-            time.sleep(0.2)
+            time.sleep(0.4)
             lol.edit_text("ꜱᴛᴀʀᴛɪɴɢ.")
             time.sleep(0.2)
             lol.edit_text("ꜱᴛᴀʀᴛɪɴɢ..")
