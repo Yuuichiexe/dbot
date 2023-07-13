a787df76d0fe901b8463c9f24a0204f76cd8f139
Anonymous <adi855053@gmail.com>
2023-07-13 12:45:18 +0530

    Update __init__.py

--- DazaiRobot/__init__.py
+++ DazaiRobot/__init__.py
@@ -49,7 +49,8 @@ if ENV:
     NO_LOAD = os.environ.get("NO_LOAD", "").split()
     START_IMG = os.environ.get(
         "START_IMG", "https://telegra.ph/file/31f2989f6306e8bbf1aa9.jpg"
-    )   
+    )
+    PM_START_IMG = os.environ.get("PM_START_IMG", None)
     STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", True))
     SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "DevilsHeavenMF")
     TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
