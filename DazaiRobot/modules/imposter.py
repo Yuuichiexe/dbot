4052361e7aed294e52347956bc7e9f0fe369fc5e
Anonymous <adi855053@gmail.com>
2023-07-16 20:22:58 +0530

    Update imposter.py

--- DazaiRobot/modules/imposter.py
+++ DazaiRobot/modules/imposter.py
@@ -3,14 +3,14 @@ from datetime import datetime
 import pytz
 from pytz import timezone
 from html import escape
-from FallenRobot.modules.mongo.sangmata_db import *
-from FallenRobot import pbot as app
-from FallenRobot.Pyro.permissions import adminsOnly
-from FallenRobot.Pyro.message_utils import kirimPesan
-from FallenRobot.utils.mongo import db as dbname
+from DazaiRobot.modules.mongo.sangmata_db import *
+from DazaiRobot import pbot as app
+from DazaiRobot.Pyro.permissions import adminsOnly
+from DazaiRobot.Pyro.message_utils import kirimPesan
+from DazaiRobot.utils.mongo import db as dbname
 
 # Set the log channel ID where name change logs will be sent
-LOG_CHANNEL_ID = -1001905357214  # Replace with the actual log channel ID
+LOG_CHANNEL_ID = -1001527287273  # Replace with the actual log channel ID
 
 @app.on_message(filters.group & ~filters.bot & ~filters.via_bot, group=3)
 async def cek_mataa(_, m):
