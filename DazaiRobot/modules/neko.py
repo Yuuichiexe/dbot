b12b0d0d17574f88c3765241c208486f6b8458b9
Anonymous <adi855053@gmail.com>
2023-07-17 19:51:21 +0530

    Update neko.py

--- DazaiRobot/modules/neko.py
+++ DazaiRobot/modules/neko.py
@@ -6,7 +6,7 @@ from PIL import Image
 from telegram import Chat
 from telegram.ext import CommandHandler
 
-from YamatoRobot import dispatcher
+from DazaiRobot import dispatcher
 
 
 def is_user_in_chat(chat: Chat, user_id: int) -> bool:
