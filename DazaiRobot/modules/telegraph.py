fba95710d523e61bf99f320001dff7bd45b8929b
Anonymous <adi855053@gmail.com>
2023-07-17 22:39:16 +0530

    Update telegraph.py

--- DazaiRobot/modules/telegraph.py
+++ DazaiRobot/modules/telegraph.py
@@ -88,9 +88,9 @@ def resize_image(image):
 
 
 __help__ = """
- ❍ /tgm :Get Telegraph Link Of Replied Media
- ❍ /tgt :Get Telegraph Link of Replied Text
- ❍ /tgt [custom name]: Get telegraph link of replied text with custom name.
+ ➛ /tgm :Get Telegraph Link Of Replied Media
+ ➛ /tgt :Get Telegraph Link of Replied Text
+ ➛ /tgt [custom name]: Get telegraph link of replied text with custom name.
 """
 
 __mod_name__ = "T-Gʀᴀᴘʜ"
