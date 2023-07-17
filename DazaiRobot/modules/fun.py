37ac9da57d3c00271b7dacf25c60a64b6513e02a
Anonymous <adi855053@gmail.com>
2023-07-17 22:26:20 +0530

    Update fun.py

--- DazaiRobot/modules/fun.py
+++ DazaiRobot/modules/fun.py
@@ -47,7 +47,7 @@ def slap(update: Update, context: CallbackContext):
     user_id = extract_user(message, args)
 
     if user_id == bot.id:
-        temp = random.choice(fun_strings.SLAP_FALLEN_TEMPLATES)
+        temp = random.choice(fun_strings.SLAP_DAZAI_TEMPLATES)
 
         if isinstance(temp, list):
             if temp[2] == "tmute":
@@ -209,20 +209,20 @@ def table(update: Update, context: CallbackContext):
 
 
 __help__ = """
- ❍ /runs*:* reply a random string from an array of replies
- ❍ /slap*:* slap a user, or get slapped if not a reply
- ❍ /shrug*:* get shrug XD
- ❍ /table*:* get flip/unflip :v
- ❍ /decide*:* Randomly answers yes/no/maybe
- ❍ /toss*:* Tosses A coin
- ❍ /bluetext*:* check urself :V
- ❍ /roll*:* Roll a dice
- ❍ /rlg*:* Join ears,nose,mouth and create an emo ;-;
- ❍ /shout <keyword>*:* write anything you want to give loud shout
- ❍ /weebify <text>*:* returns a weebified text
- ❍ /sanitize*:* always use this before /pat or any contact
- ❍ /pat*:* pats a user, or get patted
- ❍ /8ball*:* predicts using 8ball method 
+ ➛ /runs*:* reply a random string from an array of replies
+ ➛ /slap*:* slap a user, or get slapped if not a reply
+ ➛ /shrug*:* get shrug XD
+ ➛ /table*:* get flip/unflip :v
+ ➛ /decide*:* Randomly answers yes/no/maybe
+ ➛ /toss*:* Tosses A coin
+ ➛ /bluetext*:* check urself :V
+ ➛ /roll*:* Roll a dice
+ ➛ /rlg*:* Join ears,nose,mouth and create an emo ;-;
+ ➛ /shout <keyword>*:* write anything you want to give loud shout
+ ➛ /weebify <text>*:* returns a weebified text
+ ➛ /sanitize*:* always use this before /pat or any contact
+ ➛ /pat*:* pats a user, or get patted
+ ➛ /8ball*:* predicts using 8ball method 
 """
 
 SANITIZE_HANDLER = DisableAbleCommandHandler("sanitize", sanitize, run_async=True)
