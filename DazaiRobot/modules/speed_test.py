286c89dd4478586e60fb0151f0cd1a43176d1a9e
Anonymous <adi855053@gmail.com>
2023-07-17 22:38:29 +0530

    Update speed_test.py

--- DazaiRobot/modules/speed_test.py
+++ DazaiRobot/modules/speed_test.py
@@ -61,7 +61,7 @@ dispatcher.add_handler(SPEED_TEST_HANDLER)
 dispatcher.add_handler(SPEED_TEST_CALLBACKHANDLER)
 
 __help__ = """
-» /speedtest *:* Runs a speedtest and check the server speed.
+➛ /speedtest *:* Runs a speedtest and check the server speed.
 """
 
 __mod_name__ = "SᴘᴇᴇᴅTᴇsᴛ"
