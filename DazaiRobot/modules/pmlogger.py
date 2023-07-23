d06d6b36c5a99ade384690459f653b29336f523a
Anonymous <adi855053@gmail.com>
2023-07-24 00:38:56 +0530

    Update pmlogger.py

--- DazaiRobot/modules/pmlogger.py
+++ DazaiRobot/modules/pmlogger.py
@@ -8,6 +8,6 @@ from DazaiRobot import pbot as app
 @app.on_message(filters.command(["start","help"]) & filters.private)
 async def shity_af_stuff(client : Client,message : Message) :
     try : 
-        await client.send_message(-1001938782848,f"{message.from_user.mention} #Makise_NEW user just started the bot in pm")
+        await client.send_message(-1001938782848,f"{message.from_user.mention} #Dazai_NEW user just started the bot in pm")
     except Exception :
         pass
