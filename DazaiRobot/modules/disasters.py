e22a819e72e0e3f521ea61ee199784b9559e63a6
Anonymous <adi855053@gmail.com>
2023-07-27 17:41:48 +0530

    Update disasters.py

--- DazaiRobot/modules/disasters.py
+++ DazaiRobot/modules/disasters.py
@@ -646,7 +646,7 @@ dispatcher.add_handler(SUPPORTLIST_HANDLER)
 dispatcher.add_handler(SUDOLIST_HANDLER)
 dispatcher.add_handler(DEVLIST_HANDLER)
 
-__mod_name__ = "𝖣𝖾𝗏𝗌"
+__mod_name__ = "Dᴇᴠꜱ"
 __handlers__ = [
     SUDO_HANDLER,
     SUPPORT_HANDLER,
