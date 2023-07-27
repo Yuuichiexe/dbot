a048a9f3764c9f0b81eace01e4b0cc91c486ac63
Anonymous <adi855053@gmail.com>
2023-07-27 16:05:34 +0530

    Update cust_filters.py

--- DazaiRobot/modules/cust_filters.py
+++ DazaiRobot/modules/cust_filters.py
@@ -640,7 +640,7 @@ Check ➛ /markdownhelp to know more!
 
 """
 
-__mod_name__ = "Fɪʟᴛᴇʀs"
+__mod_name__ = "𝖥𝗂𝗅𝗍𝖾𝗋𝗌"
 
 FILTER_HANDLER = CommandHandler("filter", filters, run_async=True)
 STOP_HANDLER = CommandHandler("stop", stop_filter, run_async=True)
