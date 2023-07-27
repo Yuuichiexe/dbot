c996afd764cd8a8bec7ffd435d69967c80bb70f4
Anonymous <adi855053@gmail.com>
2023-07-27 15:04:19 +0530

    Update locks.py

--- DazaiRobot/modules/locks.py
+++ DazaiRobot/modules/locks.py
@@ -580,7 +580,7 @@ Locking bots will stop non-admins from adding bots to the chat.
  • Unlocking permission *pin* will allow members (non-admins) to pin a message in a group
 """
 
-__mod_name__ = "Lᴏᴄᴋs"
+__mod_name__ = "𝖫𝗈𝖼𝗄𝗌"
 
 LOCKTYPES_HANDLER = DisableAbleCommandHandler("locktypes", locktypes, run_async=True)
 LOCK_HANDLER = CommandHandler(
