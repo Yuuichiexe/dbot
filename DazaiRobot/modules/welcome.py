9a42dc345af297f95dbfb624d10809f3283536a8
Anonymous <adi855053@gmail.com>
2023-07-27 18:04:50 +0530

    Update welcome.py

--- DazaiRobot/modules/welcome.py
+++ DazaiRobot/modules/welcome.py
@@ -1111,7 +1111,7 @@ dispatcher.add_handler(CLEAN_SERVICE_HANDLER)
 dispatcher.add_handler(BUTTON_VERIFY_HANDLER)
 dispatcher.add_handler(WELCOME_MUTE_HELP)
 
-__mod_name__ = "𝖶𝖾𝗅𝖼𝗈𝗆𝖾"
+__mod_name__ = "Wᴇʟᴄᴏᴍᴇ"
 __command_list__ = []
 __handlers__ = [
     NEW_MEM_HANDLER,
