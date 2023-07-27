ec20bf62f126057e072f8a823cc7104f910ea66d
Anonymous <adi855053@gmail.com>
2023-07-27 17:24:58 +0530

    Update antiflood.py

--- DazaiRobot/modules/antiflood.py
+++ DazaiRobot/modules/antiflood.py
@@ -412,7 +412,7 @@ will result in restricting that user.
  `1w` = 1 week
  """
 
-__mod_name__ = "𝖠𝗇𝗍𝗂-𝖥𝗅𝗈𝗈𝖽"
+__mod_name__ = "Aɴᴛɪ-ꜰʟᴏᴏᴅ"
 
 FLOOD_BAN_HANDLER = MessageHandler(
     Filters.all & ~Filters.status_update & Filters.chat_type.groups,
