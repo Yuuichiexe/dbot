31ccb6d452bef4cb8af72d174080b363c44d41f6
Anonymous <adi855053@gmail.com>
2023-07-27 18:02:46 +0530

    Update userinfo.py

--- DazaiRobot/modules/userinfo.py
+++ DazaiRobot/modules/userinfo.py
@@ -523,7 +523,7 @@ dispatcher.add_handler(GET_BIO_HANDLER)
 dispatcher.add_handler(SET_ABOUT_HANDLER)
 dispatcher.add_handler(GET_ABOUT_HANDLER)
 
-__mod_name__ = "𝖨𝗇𝖿𝗈𝗌"
+__mod_name__ = "Uꜱᴇʀꜱ-ɪɴꜰᴏ"
 __command_list__ = ["setbio", "bio", "setme", "me", "info"]
 __handlers__ = [
     ID_HANDLER,
