bc93754cc87456cd1637a6f7f1b1faf7c2ceb630
Anonymous <adi855053@gmail.com>
2023-07-30 17:30:55 +0530

    Update games.py

--- DazaiRobot/modules/games.py
+++ DazaiRobot/modules/games.py
@@ -1,33 +1,39 @@
 from telethon import events, Button, custom
-from AliciaRobot.events import register
+from DazaiRobot.events import register
 
 @register(pattern=("^/(game|games)"))
 async def games(event):
 
     await event.reply(
-                    "hello, we have attched some games for you, press the button and play games.",
+                    "𝗁𝖾𝗅𝗅𝗈, 𝗐𝖾 𝗁𝖺𝗏𝖾 𝖺𝗍𝗍𝖼𝗁𝖾𝖽 𝗌𝗈𝗆𝖾 𝗀𝖺𝗆𝖾𝗌 𝖿𝗈𝗋 𝗒𝗈𝗎, 𝗉𝗋𝖾𝗌𝗌 𝗍𝗁𝖾 𝖻𝗎𝗍𝗍𝗈𝗇 𝖺𝗇𝖽 𝗉𝗅𝖺𝗒 𝗀𝖺𝗆𝖾𝗌",
                     buttons=[
                         [
                             Button.url(
-                                "BUBBLE TOWER 3D",
+                                "ʙᴜʙʙʟᴇ ᴛᴏᴡᴇʀ 3ᴅ",
                                 "https://play.famobi.com/bubble-tower-3d",
                             ),
                             Button.url(
-                                "ROM NOM RUN",
+                                "ʀᴏᴍ ɴᴏᴍ ʀᴜɴ",
                                 "https://play.famobi.com/om-nom-run",
                             ),
                         ],
 
                         [
                             Button.url(
-                                "CANNON BALLS 3D",
+                                "ᴄᴀɴᴏɴ ʙᴀʟʟs 3ᴅ",
                                 "https://play.famobi.com/cannon-balls-3d",
                             ),
                             Button.url(
-                                "ARCHERY WORLD TOUR",
+                                "ᴀʀᴄʜᴇʀʏ ᴡᴏᴏᴅ ᴛᴏᴜʀ",
                                 "https://play.famobi.com/archery-world-tour",
                             ),
                         ]
                     ],
                     parse_mode="html",
                 )
+
+__mod_name__ = "𝙶ᴀᴍᴇs"
+
+__help__ = """
+> /games : Some games for ur boredom
+"""
