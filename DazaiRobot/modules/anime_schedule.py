c7fb45331ab6df805126c0ea7894283e1002e949
Anonymous <adi855053@gmail.com>
2023-08-04 10:28:12 +0530

    Update anime_schedule.py

--- DazaiRobot/modules/anime_schedule.py
+++ DazaiRobot/modules/anime_schedule.py
@@ -25,7 +25,7 @@ def schedule(_, message):
     message.reply_text(f"**Today's Schedule:**\nTime-Zone: Tokyo (GMT +9)\n\n{text}")
 
 
-__mod_name__ = "sᴄʜᴇᴅᴜʟᴇ"
+__mod_name__ = "𝚂ᴄʜᴇᴅᴜʟᴇ"
 
 __help__ = """
 ➛ `/latest`: to see latest anime episode
