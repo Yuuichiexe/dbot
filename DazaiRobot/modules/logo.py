6919400e75f2daec44ad0fbaf8e878ca7b75b4b6
Anonymous <adi855053@gmail.com>
2023-08-04 10:59:12 +0530

    Update logo.py

--- DazaiRobot/modules/logo.py
+++ DazaiRobot/modules/logo.py
@@ -319,7 +319,7 @@ async def lego(event):
             os.remove(fname)
 
 
-__mod_name__ = "ʟᴏɢᴏ"
+__mod_name__ = "𝙻ᴏɢᴏ"
 
 __help__ = """
 I can create some beautiful and attractive logo for your profile pics.
