6bad0830059f7208c4d7c48dcfad649b5a396da5
Anonymous <adi855053@gmail.com>
2023-08-04 10:51:34 +0530

    Update google.py

--- DazaiRobot/modules/google.py
+++ DazaiRobot/modules/google.py
@@ -264,7 +264,7 @@ async def apk(e):
         await e.reply("Exception Occured:- " + str(err))
 
 
-__mod_name__ = "Gᴏᴏɢʟᴇ"
+__mod_name__ = "𝙶ᴏᴏɢʟᴇ"
 
 __help__ = """
  ➛ /google <text>*:* Perform a google search
