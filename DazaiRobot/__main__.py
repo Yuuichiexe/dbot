7dc50b8e13251e2b866641551a612c97f547d3c6
Anonymous <adi855053@gmail.com>
2023-08-19 21:31:04 +0530

    Update __main__.py

--- DazaiRobot/__main__.py
+++ DazaiRobot/__main__.py
@@ -185,6 +185,7 @@ def test(update: Update, context: CallbackContext):
     print(update.effective_message)
     
 
+@Dispatcher.run_async
 def start(update: Update, context: CallbackContext):
     args = context.args
     uptime = get_readable_time((time.time() - StartTime))
