24c81ea09c65ef884fdcaebb5c8aac15199e3304
Anonymous <adi855053@gmail.com>
2023-09-03 17:34:07 +0530

    Update imgrev.py

--- DazaiRobot/modules/imgrev.py
+++ DazaiRobot/modules/imgrev.py
@@ -31,13 +31,15 @@ class STRINGS:
     UNSUPPORTED_MEDIA_TYPE: str = "⚠️ <b>Unsupported media type!</b>\nℹ️ Please reply with a supported media type: image, sticker, or image file."
     
     DOWNLOADING_MEDIA: str = "𝖲𝖾𝖺𝗋𝖼𝗁𝗂𝗇𝗀..."
-    UPLOADING_TO_API_SERVER: str = "𝖴𝗉𝗅𝗈𝖺𝖽𝗂𝗇𝗀 𝗆𝖾𝖽𝗂𝖺 𝗍𝗈 <b>𝖠𝖯𝖨 𝗌𝖾𝗋𝗏𝖾𝗋</b>... 📶"
+    UPLOADING_TO_API_SERVER: str = "𝖴𝗉𝗅𝗈𝖺𝖽𝗂𝗇𝗀 𝗆𝖾𝖽𝗂𝖺 𝗍𝗈 <b>𝖠𝖯𝖨 𝗌𝖾𝗋𝗏𝖾𝗋</b>..."
     PARSING_RESULT: str = "𝖦𝖺𝗍𝗁𝖾𝗋𝗂𝗇𝗀 𝗂𝗇𝖿𝗈𝗋𝗆𝖺𝗍𝗂𝗈𝗇..."
     
     EXCEPTION_OCCURRED: str = "🚫 <b>Exception occurred!</b>\n\n<b>Exception:</b> {}"
     
     RESULT: str = """
     <b>𝖱𝖾𝗌𝗎𝗅𝗍𝗌 𝖿𝗈𝗋</b> <code>{query}</code>
+
+    <b>𝖢𝗋𝖾𝖽𝗂𝗍𝗌:</b> @KangersNetwork
     """
     OPEN_PAGE: str = "⎋ 𝖫𝗂𝗇𝗄 ⎋"
 
