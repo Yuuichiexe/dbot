c187764de21a237d458592123bdf58998db457ce
Anonymous <adi855053@gmail.com>
2023-09-29 22:29:05 +0530

    Update couple.py

--- DazaiRobot/modules/couple.py
+++ DazaiRobot/modules/couple.py
@@ -94,8 +94,8 @@ async def ctest(_, message):
 
          draw = ImageDraw.Draw(img)
 
-         img.paste(img1, (110, 285), img1)
-         img.paste(img2, (1280, 280), img2)
+         img.paste(img1, (108, 281), img1)
+         img.paste(img2, (1282, 282), img2)
 
          img.save(f'test_{cid}.png')
     
