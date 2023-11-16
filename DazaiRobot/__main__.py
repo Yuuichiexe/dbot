12bd17ce158613612383f0075b8d35fc25a1a1e4
Anonymous <adi855053@gmail.com>
2023-11-16 14:20:58 +0530

    Update __main__.py

--- DazaiRobot/__main__.py
+++ DazaiRobot/__main__.py
@@ -12,8 +12,6 @@ from typing import Optional
 from pyrogram import Client, idle, filters
 
 import DazaiRobot.modules.sql.users_sql as sql
-from DazaiRobot.modules.sudoers import bot_sys_stats as bss
-
 from DazaiRobot import (ALLOW_EXCL, LOGGER,
                           OWNER_ID, SUPPORT_CHAT, TOKEN,
                           SUPPORT_CHAT, dispatcher, StartTime, telethn, updater, pbot)
